66ac9f31b295b66a3927c87861a3617ee7798ec8
Marv <marvin4alarcon3@gmail.com>
2019-06-20 14:39:55 -0600

    Creacion de plantillas para el diseño

---
+++ templates/footer.php
@@ -0,0 +1,30 @@
+<footer class="footer">
+    <div class="content has-text-centered">
+        <p>
+            Marv3!
+        </p>
+    </div>
+</footer>
+<script src="http://<?php echo $_SERVER['SERVER_NAME']; ?>/SchoolFinalTest/js/jquery-3.4.0.js"></script>
+<script>
+var current = window.location.href; 
+        var lista = document.querySelector("#lista");
+    
+    if(current.includes('/control/')) {    
+        
+        
+        lista.querySelector("#two").classList.add("is-active");
+        
+    } else if (current.includes("/cursos/")) {
+         
+         lista.querySelector("#three").classList.add("is-active");
+    
+    } else {
+
+         lista.querySelector("#one").classList.add("is-active");
+    }
+ 
+</script>
+</body>
+
+</html>
